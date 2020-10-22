93d0172390d4ed72f15987291b74950f903e5822
krae <kraewind@gmail.com>
2020-10-22 12:15:27 -0400

    commit tree 1b28a878f7f4b6de8f23ab327418b9939ebea898

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array[0]
+    elsif array.size == 2
+        final = array[0] + " and " + array[1]
+        final
+    else
+        last_item = array.last
+        array.pop
+        thing = array.join(", ")
+        return thing + "," + " and " + last_item
+    end
 end
